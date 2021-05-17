e422710ae8e635365fcbee845d1eb8b624462f64
Miguel Casqueira <miguel@balena.io>
2021-05-17 17:57:40 -0400

    z

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const github = require('@actions/github');
 
 try {
   const time = (new Date()).toTimeString();
-  core.setOutput("time", (Math.random() * 1000000).toFixed(0));
-  core.setOutput("releaseId", (Math.random() * 1000000).toFixed(0));
+  core.setOutput("timeTo", (Math.random() * 1000000).toFixed(0));
+  core.setOutput("release", (Math.random() * 1000000).toFixed(0));
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
